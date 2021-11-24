6e0e1f60ab1ed579d1148dbc05630fbff2996faa
magskwa <magali.egger@heig-vd.ch>
2021-11-24 10:55:23 +0100

    classe personne

--- .idea/src/model/Group.java
+++ .idea/src/model/Group.java
@@ -1,4 +1,5 @@
 package model;
 
 public class Group {
+    private String
 }
--- .idea/src/model/Person.java
+++ .idea/src/model/Person.java
@@ -1,4 +1,33 @@
 package model;
 
 public class Person {
+
+    private String nom;
+    private String prenom;
+    private String email;
+
+    public Person(String nom, String prenom, String email){
+        this.nom = nom;
+        this.prenom = prenom;
+        this.email = email;
+    }
+
+    // permet de crée une personne a partir de son adresse email
+    public Person(String email){
+        this.email = email;
+        this.prenom = email.substring(0, email.lastIndexOf("."));
+        this.nom = email.substring(email.lastIndexOf("."), email.lastIndexOf("@"));
+    }
+
+    public String getPrenom(){
+        return prenom;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public String getEmail() {
+        return email;
+    }
 }
